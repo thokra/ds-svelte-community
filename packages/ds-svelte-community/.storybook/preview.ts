600679dcea9e1b934a158f5a818e4224965437f2
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2023-12-19 11:18:56 +0100

    this time I think it's fixed

--- packages/ds-svelte-community/.storybook/preview.ts
+++ packages/ds-svelte-community/.storybook/preview.ts
@@ -1,15 +1,11 @@
 // import "@navikt/ds-css/index.css";
 // import "@navikt/ds-tokens/dist/tokens.css";
 import type { Preview } from "@storybook/svelte";
-import { themes } from "@storybook/theming";
 import "../src/lib/css/index.css";
 import "./custom.css";
 
 const preview: Preview = {
 	parameters: {
-		docs: {
-			theme: themes.dark,
-		},
 		actions: { argTypesRegex: "^on[A-Z].*" },
 		controls: {
 			matchers: {
