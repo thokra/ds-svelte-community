a3e0dac01785c16f850239db199bb7c4bb7b56ef
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2024-08-01 15:11:48 +0200

    Only show subset of unions by default

--- packages/ds-svelte-community/src/doclib/TypeRenderer.svelte
+++ packages/ds-svelte-community/src/doclib/TypeRenderer.svelte
@@ -1,30 +1,58 @@
 <script lang="ts">
 	import { dev } from "$app/environment";
+	import Button from "$lib/components/Button/Button.svelte";
 	import type { Type } from "@nais/vite-plugin-svelte-docs";
 
-	let { type: typ }: { type: Type } = $props();
+	let { type: typ, show = true }: { type: Type; show?: boolean } = $props();
+
+	const maxItems = 10;
+	let shouldShowAll = $derived(
+		(!Array.isArray(typ) && typ.type == "union" && typ.values.length < maxItems) ||
+			(!Array.isArray(typ) && typ.type != "union") ||
+			(Array.isArray(typ) && typ.length < maxItems),
+	);
+
+	let showAll = $state(false);
 </script>
 
-{#if Array.isArray(typ)}
-	{#each typ as t}
-		<svelte:self type={t} />
-	{/each}
-{:else if typ.type == "union"}
-	{#each typ.values as t}
-		<svelte:self type={t} />
-	{/each}
-{:else if ["string", "number", "boolean", "undefined", "null"].includes(typ.type)}
-	<code>{typ.type}</code>
-{:else if typ.type == "literal"}
-	<code>{typ.value}</code>
-{:else if typ.type == "interface"}
-	<code>{typ.name}</code>
-{:else if typ.type == "unknown"}
-	<code>unknown</code>
-{:else if typ.type == "snippet"}
-	<code>Snippet</code>
-{:else}
-	<code class:err={dev}>{typ.type}</code>
+{#if show}
+	{#if Array.isArray(typ)}
+		{#each typ as t, i}
+			<svelte:self type={t} show={shouldShowAll || showAll || i < maxItems} />
+		{/each}
+	{:else if typ.type == "union"}
+		{#each typ.values as t, i}
+			<svelte:self type={t} show={shouldShowAll || showAll || i < maxItems} />
+		{/each}
+	{:else if ["string", "number", "boolean", "undefined", "null"].includes(typ.type)}
+		<code>{typ.type}</code>
+	{:else if typ.type == "literal"}
+		<code>{typ.value}</code>
+	{:else if typ.type == "interface"}
+		<code>{typ.name}</code>
+	{:else if typ.type == "unknown"}
+		<code>unknown</code>
+	{:else if typ.type == "snippet"}
+		<code>Snippet</code>
+	{:else}
+		<code class:err={dev}>{typ.type}</code>
+	{/if}
+{/if}
+
+{#if !shouldShowAll}
+	<Button
+		variant="tertiary-neutral"
+		size="small"
+		onclick={() => (showAll = !showAll)}
+		style="display: inline;"
+	>
+		...
+		{#if showAll}
+			Hide
+		{:else}
+			Show all
+		{/if}
+	</Button>
 {/if}
 
 <style>
